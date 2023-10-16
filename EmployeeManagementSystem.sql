404ace1d2f2f21a02f5f72b77600c84401ac4759
Rokonuzzaman-Mridul <97906039+Rokonuzzaman-Mridul@users.noreply.github.com>
2023-10-17 00:50:03 +0600

    Create EmployeeManagementSystem.sql

---
+++ EmployeeManagementSystem.sql
@@ -0,0 +1,20 @@
+/*CREATE DATABASE EmployeeManagement;
+
+USE EmployeeManagement;
+
+CREATE TABLE Employee (
+    emp_ID INT PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    gender VARCHAR(10),
+    age INT,
+    contact_add VARCHAR(255),
+    dob DATE
+);
+
+CREATE TABLE JobDepartment (
+    Dept_ID INT PRIMARY KEY,
+    Dept_name VARCHAR(30),
+    tot_emp int,
+    salary_range VARCHAR(30)
+);*/
